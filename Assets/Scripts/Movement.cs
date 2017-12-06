087d91b848ee587dd3c0b39d2e62b50bd6f0b764
KReguieg <Khaled.Reguieg@gmail.com>
2017-12-06 21:31:17 +0100

    Initial Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float speed;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed;
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.up * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    enum PowerUpEffects : int { Giant, Mouse, HighDamage, HighSpeed, IronManFlight};
+    enum PowerUpTimers : int { Giant=10, Mouse=10, HighDamage=20, HighSpeed=20, IronManFlight=30};
+
+    public GameObject pickupEffect;
+
+    private GameObject player;
+
+    private float timer;
+
+    private float _powerUpTimer = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            player = other.gameObject;
+            PickUp();
+        }
+
+        timer += Time.deltaTime;
+        if(timer >= _powerUpTimer)
+        {
+            DetachPowerUpEffectFromPlayer();
+            timer = 0f;
+        }
+    }
+
+    void PickUp()
+    {
+        Debug.Log("PowerUp picked up!");
+
+        // Spawn cool effect
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        // Apply effect to the palyer
+        ApplyPowerUpEffectOnPlayer((int) PowerUpEffects.Giant, (float) PowerUpTimers.Giant);
+
+        // Remove PowerUp object
+        Destroy(gameObject);
+    }
+
+    private void ApplyPowerUpEffectOnPlayer(int effect, float powerUpTimer)
+    {
+        // template power growth
+        Debug.Log("Massive Hulk now!!!");
+        _powerUpTimer = powerUpTimer;
+        player.GetComponent<MeshRenderer>().material.color = Color.green;
+        player.transform.localScale += Vector3.one*2.5f;
+    }
+
+    private void DetachPowerUpEffectFromPlayer()
+    {
+        // undo hulkmodus
+        Debug.Log("Dr. Benner again!!!");
+        player.GetComponent<MeshRenderer>().material.color = Color.white;
+        player.transform.localScale = Vector3.one;
+    }
+}
